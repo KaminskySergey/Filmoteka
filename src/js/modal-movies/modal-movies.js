08ff3f8bfb551f1c6086d6b97b054ddf34ebcc36
Inna <isko.inna.94@gmail.com>
2022-12-08 21:15:23 +0200

    Update modal-movies.js

--- src/js/modal-movies/modal-movies.js
+++ src/js/modal-movies/modal-movies.js
@@ -7,7 +7,7 @@ const getRef = selector => document.querySelector(selector);
 const API_KEY = '102d4305e0abdbf0fd48836d5abb1978';
 
 let movieID = '';
-export let movieInfo = '';
+let movieInfo = '';
 let instance;
 let moviePoster;
 let movieTitle;
