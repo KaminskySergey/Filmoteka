9220111f3a6e7ca08a17e89338b2cafadab95dfb
Lisovyi-Vladyslav <dragodrift2005@gmail.com>
2022-12-08 18:03:25 +0200

    filter-button

--- src/js/gallery/poular_movie.js
+++ src/js/gallery/poular_movie.js
@@ -49,4 +49,4 @@ function decodeGenres([...args]) {
   return genres.join(', ');
 }
 
-export { fetchGenres, decodeGenres, IMG_URL, gallery};
+export { fetchGenres, decodeGenres, IMG_URL, gallery, createMoviesList};
