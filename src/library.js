edaf225fe10f2e4a1b2a90037b9fc07c67094a4c
OksanaMashtaler <mashoksana@gmail.com>
2023-01-22 10:24:27 +0200

    fix

---
+++ src/library.js
@@ -0,0 +1,29 @@
+// header ========================
+import './js/header/header';
+// import './js/fiter-button-header/filter-button-header';
+import './js/up-button/_up-button';
+import './js/firebase';
+
+// gallery ===========================
+//import './js/pagination/load_page';
+
+import './js/gallery/poular_movie';
+
+// pagination=============
+// import './js/pagination/paginatin_search';
+// import './js/pagination/pagination_genres';
+// import './js/pagination/pagination';
+
+// feedback modal
+import './js/feedback-modal/_feedback-modal';
+
+// footer ==============================
+import './js/footer/footer-modal';
+import './js/footer/modal-team';
+import './js/footer/footer-modal-team';
+
+// modal movies ==============================
+import './js/modal-movies/modal-movies';
+import './js/modal-movies/modal-movie-buttons';
+
+// import './js/modal-movies/modal-movie-buttons';
